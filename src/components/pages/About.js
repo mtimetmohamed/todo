9cb700f7dd0a62b494326f2daa476c23a4a30c82
mtimetmohamed <44278962+mtimetmohamed@users.noreply.github.com>
2020-02-12 03:06:06 -0800

    Initial commit

---
+++ src/components/pages/About.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function About() {
+  return (
+    <React.Fragment>
+      <h1>About</h1>
+      <p>TodoList</p>
+    </React.Fragment>
+  )
+}
+
+
+export default About;
